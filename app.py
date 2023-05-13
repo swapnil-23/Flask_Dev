1238f24b202d4a99226338281eebe29b849e9269
SWAPNIL BISWAS <73814033+swapnil-23@users.noreply.github.com>
2023-05-13 09:17:53 +0530

    everything added

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app=Flask(__name__)
 def welcom():
     return 'Welcome'
 
+@app.route('/members')
+def memebers():
+    return 'Welcome members'
+
+
 if __name__=='__main__':
     app.run(debug=True)
